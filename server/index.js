ed49b4e067178d25b6f5dd92693bd1fecfe017de
DavidS2003 <sanchez.david2003@gmail.com>
2026-04-04 14:40:06 -0400

    Full stack app up and running

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const app = express();
+
+const path = require('path');
+
+const db = require('./queries');
+
+const PORT = 9001;
+
+
+app.use(express.static(path.resolve(__dirname, '../client/dist')));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'));
+});
+
+// app.get('/test', (req, res)=>(
+// // do smt here
+// ));
+
+app.get('/links', db.getLinks);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
